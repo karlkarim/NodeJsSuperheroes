4d562ec05b834087ab0cd727d725438eae160ab3
Opilane TTHK <opilane@tthk.ee>
2020-02-12 15:19:52 +0200

    teine kord

---
+++ server.js
@@ -0,0 +1,12 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+
+
+
+supervillains.all.forEach(element => console.log(element));
+console.log(supervillains.all.length);
+
+
+// const express = require("express");
+// const app = express();
